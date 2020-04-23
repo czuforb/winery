ba18765185452bd6d62b8607671432a4944d361d
czuforb <czuforbence@gmail.com>
2020-04-23 17:00:05 +0200

    .......::

--- functions/barion.js
+++ functions/barion.js
@@ -3,47 +3,64 @@ require("dotenv").config({
 })
 
 exports.handler = function (event, context, callback) {
-  let orderPayment = {
-    OrderNumber: "O-2019-0001",
-    PaymentRequestId: "O-2019-0001-1",
+  const axios = require("axios")
+
+  const data = {
+    POSKey: process.env.BARION_POS,
     PaymentType: "Immediate",
+    PaymentWindow: "00:30:00",
+
+    GuestCheckout: "True",
+    FundingSources: ["All"],
+
+    PaymentRequestId: "payment-25",
+    OrderNumber: "order-25",
+    PayerHint: "joseph-schmidt@example.com",
+    ShippingAddress: {
+      Country: "AT",
+      City: "Vienna",
+      Region: "",
+      Zip: "1234",
+      Street: "13 Etwas Strasse",
+      Street2: "",
+      FullName: "Joseph Schmidt",
+      Phone: "43259123456789",
+    },
+
+    RedirectUrl: "http://shop.example.com/danke-fur-den-kauf",
+    CallbackUrl: "http://shop.example.com/api/callback",
+
+    Locale: "de-AT",
+    Currency: "EUR",
+
     Transactions: [
       {
-        POSTransactionId: "O-2019-0001",
-        Payee: "info@example.com",
-        Total: 210,
+        POSTransactionId: "tr-25",
+        Payee: "eshop@example.com",
+        Total: 400,
         Items: [
           {
-            Name: "Egg",
-            Description: "Child of chicken",
-            Quantity: 3,
+            Name: "Digital Camera",
+            Description: "Canon D500",
+            Quantity: 1,
             Unit: "pcs",
-            UnitPrice: 70,
-            ItemTotal: 210,
+            UnitPrice: 300,
+            ItemTotal: 300,
+            SKU: "cn-d500-fxc3",
+          },
+          {
+            Name: "SD Card",
+            Description: "SanDisk SD mini 512GB - 3 year garantee",
+            Quantity: 2,
+            Unit: "pcs",
+            UnitPrice: 50,
+            ItemTotal: 100,
+            SKU: "snd-sd-500gm",
           },
         ],
       },
     ],
-    ShippingAddress: {
-      FullName: "Andrew Jones",
-      Zip: "1000",
-      City: "Budapest",
-      Street: "Kossuth Street 2.",
-    },
-    Currency: "HUF",
-    RedirectUrl: "https://google.com",
-    CallbackUrl: "https://google.com",
   }
-  // 1) Import the 'node-barion' module
-  const Barion = require("node-barion")
 
-  // 2) Instantiate a Barion object
-  let barion = new Barion({
-    POSKey: process.env.BARION_POS,
-    Environment: "test",
-  })
-
-  barion.startPayment(orderPayment, function (err, data) {
-    console.log(data)
-  })
+  axios.post("https://api.test.barion.com/v2/Payment/Start", data)
 }
