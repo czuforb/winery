897d409c064578681809ea1224959d5ce0cc07c4
czuforb <czuforbence@gmail.com>
2020-05-13 16:40:04 +0200

    netlifymailgun

--- functions/sendorder.js
+++ functions/sendorder.js
@@ -4,23 +4,23 @@
 
 exports.handler = function (event, _context, callback) {
   console.log(event.body)
-  //   const mailgun = require("mailgun.js")
-  //   var mg = mailgun.client({
-  //     username: "api",
-  //     key: process.env.MAILGUN_API_KEY,
-  //     url: "https://api.eu.mailgun.net",
-  //   })
+  const mailgun = require("mailgun.js")
+  var mg = mailgun.client({
+    username: "api",
+    key: process.env.MAILGUN_API_KEY,
+    url: "https://api.eu.mailgun.net",
+  })
 
-  //   const data = JSON.stringify(event.body)
-  //   mg.messages
-  //     .create("czuforbence.com", {
-  //       from: "Excited User <hello@czuforbence.com>",
-  //       to: ["czuforbence@gmail.com"],
-  //       subject: "Hello",
-  //       text: "Testing some Mailgun awesomness!",
-  //       template: "test",
-  //       "h:X-Mailgun-Variables": event.body,
-  //     })
-  //     .then(msg => console.log(msg)) // logs response data
-  //     .catch(err => console.log(err)) // logs any error
+  // const data = JSON.stringify(event.body)
+  mg.messages
+    .create("czuforbence.com", {
+      from: "Excited User <hello@czuforbence.com>",
+      to: ["czuforbence@gmail.com"],
+      subject: "Hello",
+      text: "Testing some Mailgun awesomness!",
+      template: "test",
+      "h:X-Mailgun-Variables": event.body,
+    })
+    .then(msg => console.log(msg, "RESPOSZNVE")) // logs response data
+    .catch(err => console.log(err, "ERR AMONNAN")) // logs any error
 }
